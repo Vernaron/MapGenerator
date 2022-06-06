67b5d59dccc0708fb612e461a5fb4e8fd84f8da1
Lyzander Johnson <lyzanderjohnson@gmail.com>
2022-06-06 11:18:23 -0400

    Added first function definition

--- MapFuncs.h
+++ MapFuncs.h
@@ -1,9 +1,8 @@
 #ifndef MAPFUNC_H
 #define MAPFUNC_H
 #pragma once
+#include "mapBuild.h"
 
-#include <iostream>
-#include <string>
 void HelloWorld(std::string hi);
 
 #endif
--- mapBuild.h
+++ mapBuild.h
@@ -1,5 +1,6 @@
 #ifndef MAPBUILD_H
 #define MAPBUILD_H
-
-
+#include <iostream>
+#include <string>
+std::string** makeArr(int numCountries, int x, int y, int dangerAreas, int heightFactor);
 #endif
